8f03df6a820df0d43866e27e60f34e96873397aa
reggiemcdonald <regmcdonald95@gmail.com>
2020-07-03 16:13:56 -0600

    internal: Add instrumentation
    This commit adds an instrumentation file to the internal package which will contain the instruments that will be used for collecting metrics.

---
+++ internal/instruments.go
@@ -0,0 +1,44 @@
+package internal
+
+import (
+	"go.opentelemetry.io/otel/api/global"
+	"go.opentelemetry.io/otel/api/metric"
+	"go.opentelemetry.io/otel/api/unit"
+	"log"
+)
+
+type openTelemetryInstrumentation struct {
+	// Bytes Written Metric
+	WriteCount metric.Int64Counter
+	// Number of new connections
+	NewConnectionsCount metric.Int64Counter
+}
+
+var Instruments *openTelemetryInstrumentation = initInstruments()
+
+func initInstruments() *openTelemetryInstrumentation {
+	meter := global.Meter("github.com/go-redis/redis")
+
+	writeCount, err := meter.NewInt64Counter("redis.num_writes",
+		metric.WithDescription("the number of writes initiated"),
+		metric.WithUnit(unit.Bytes),
+	)
+	if err != nil {
+		// TODO: handle errors
+		log.Printf("failed to create instrument WriteCount")
+	}
+
+	newConnectionsCount, err := meter.NewInt64Counter("redis.num_new_conn",
+		metric.WithDescription("the number of new connections created"),
+	)
+	if err != nil {
+		// TODO: handle errors
+		log.Printf("failed to create instrument NumNewConnections")
+	}
+
+	return &openTelemetryInstrumentation{
+		WriteCount:          writeCount,
+		NewConnectionsCount: newConnectionsCount,
+	}
+}
+
