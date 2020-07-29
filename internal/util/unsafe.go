00803e10330833da61c6942d61f0993ac08d2627
zhangsheng <zhangsheng@mafengwo.com>
2020-07-29 10:13:41 +0800

    byte切片和string类型互转，通过unsafe包节省内存及内存回收压力

--- internal/util/unsafe.go
+++ internal/util/unsafe.go
@@ -6,11 +6,22 @@ import (
 	"unsafe"
 )
 
+/**
+通过unsafe包将byte切片和字符串互相转化，而且避免了产生新的内存申请。
+这在需要大量处理byte切片转为字符串的应用场景中可以有很好的作用。
+比如消息队列，大量减少小内存的申请减少GC的回收压力，对高并发的应用有一定程度的优化作用。
+*/
+
+//byte切片转字符串常规方式，会增加内存使用以及内存回收压力
+//b := []byte{'a','b','c'}
+//s := string(b) // 显示转换为字符串
 // BytesToString converts byte slice to string.
 func BytesToString(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
 
+//string转byte切片常规方式
+//cmdByte := []byte(cmdString)
 // StringToBytes converts string to byte slice.
 func StringToBytes(s string) []byte {
 	return *(*[]byte)(unsafe.Pointer(
