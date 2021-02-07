66f0a04945f7be4e60a00d97317681d051c0d06f
renzheng.wang <renzheng.wang@ucloud.cn>
2021-02-07 15:13:34 +0800

    add comment

--- internal/rand/rand.go
+++ internal/rand/rand.go
@@ -44,4 +44,7 @@ func (s *source) Seed(seed int64) {
 	s.mu.Unlock()
 }
 
+// Shuffle pseudo-randomizes the order of elements.
+// n is the number of elements.
+// swap swaps the elements with indexes i and j.
 func Shuffle(n int, swap func(i, j int)) { pseudo.Shuffle(n, swap) }
