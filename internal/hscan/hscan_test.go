27df2313cd171289a11d62a1803169f7d73d2023
monkey92t <shaohua@zongheng.com>
2021-02-09 20:44:24 +0800

    hscan adds support for i386 platform (#1652)
    * hscan adds support for i386 platform
    
    Signed-off-by: monkey <monkey92t@gmail.com>
    
    * update go.sum
    
    Signed-off-by: monkey <monkey92t@gmail.com>
    
    * detect overflow of scan
    
    Signed-off-by: monkey <golang@88.com>
    
    * restore the return value type of decoderFunc to error
    
    Signed-off-by: monkey <golang@88.com>
    
    * fix clean
    
    Signed-off-by: monkey <golang@88.com>
    
    * fix clean
    
    Signed-off-by: monkey <golang@88.com>
    
    Co-authored-by: Monkey <monkey@MonkeydeiMac.local>

--- internal/hscan/hscan_test.go
+++ internal/hscan/hscan_test.go
@@ -1,6 +1,8 @@
 package hscan
 
 import (
+	"math"
+	"strconv"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -11,12 +13,21 @@ type data struct {
 	Omit  string `redis:"-"`
 	Empty string
 
-	String string  `redis:"string"`
-	Bytes  []byte  `redis:"byte"`
-	Int    int     `redis:"int"`
-	Uint   uint    `redis:"uint"`
-	Float  float32 `redis:"float"`
-	Bool   bool    `redis:"bool"`
+	String  string  `redis:"string"`
+	Bytes   []byte  `redis:"byte"`
+	Int     int     `redis:"int"`
+	Int8    int8    `redis:"int8"`
+	Int16   int16   `redis:"int16"`
+	Int32   int32   `redis:"int32"`
+	Int64   int64   `redis:"int64"`
+	Uint    uint    `redis:"uint"`
+	Uint8   uint8   `redis:"uint8"`
+	Uint16  uint16  `redis:"uint16"`
+	Uint32  uint32  `redis:"uint32"`
+	Uint64  uint64  `redis:"uint64"`
+	Float   float32 `redis:"float"`
+	Float64 float64 `redis:"float64"`
+	Bool    bool    `redis:"bool"`
 }
 
 type i []interface{}
@@ -43,6 +54,47 @@ var _ = Describe("Scan", func() {
 		Expect(Scan(data{}, i{"key", "string"}, i{nil, nil})).To(HaveOccurred())
 	})
 
+	It("number out of range", func() {
+		f := func(v uint64) string {
+			return strconv.FormatUint(v, 10) + "1"
+		}
+		keys := i{"int8", "int16", "int32", "int64", "uint8", "uint16", "uint32", "uint64", "float", "float64"}
+		vals := i{
+			f(math.MaxInt8), f(math.MaxInt16), f(math.MaxInt32), f(math.MaxInt64),
+			f(math.MaxUint8), f(math.MaxUint16), f(math.MaxUint32), strconv.FormatUint(math.MaxUint64, 10) + "1",
+			"13.4028234663852886e+38", "11.79769313486231570e+308",
+		}
+		for k, v := range keys {
+			var d data
+			Expect(Scan(&d, i{v}, i{vals[k]})).To(HaveOccurred())
+		}
+
+		//success
+		f = func(v uint64) string {
+			return strconv.FormatUint(v, 10)
+		}
+		keys = i{"int8", "int16", "int32", "int64", "uint8", "uint16", "uint32", "uint64", "float", "float64"}
+		vals = i{
+			f(math.MaxInt8), f(math.MaxInt16), f(math.MaxInt32), f(math.MaxInt64),
+			f(math.MaxUint8), f(math.MaxUint16), f(math.MaxUint32), strconv.FormatUint(math.MaxUint64, 10),
+			"3.40282346638528859811704183484516925440e+38", "1.797693134862315708145274237317043567981e+308",
+		}
+		var d data
+		Expect(Scan(&d, keys, vals)).NotTo(HaveOccurred())
+		Expect(d).To(Equal(data{
+			Int8:    math.MaxInt8,
+			Int16:   math.MaxInt16,
+			Int32:   math.MaxInt32,
+			Int64:   math.MaxInt64,
+			Uint8:   math.MaxUint8,
+			Uint16:  math.MaxUint16,
+			Uint32:  math.MaxUint32,
+			Uint64:  math.MaxUint64,
+			Float:   math.MaxFloat32,
+			Float64: math.MaxFloat64,
+		}))
+	})
+
 	It("scans good values", func() {
 		var d data
 
@@ -50,16 +102,22 @@ var _ = Describe("Scan", func() {
 		Expect(Scan(&d, i{"key"}, i{"value"})).NotTo(HaveOccurred())
 		Expect(d).To(Equal(data{}))
 
-		keys := i{"string", "byte", "int", "uint", "float", "bool"}
-		vals := i{"str!", "bytes!", "123", "456", "123.456", "1"}
+		keys := i{"string", "byte", "int", "int64", "uint", "uint64", "float", "float64", "bool"}
+		vals := i{
+			"str!", "bytes!", "123", "123456789123456789", "456", "987654321987654321",
+			"123.456", "123456789123456789.987654321987654321", "1",
+		}
 		Expect(Scan(&d, keys, vals)).NotTo(HaveOccurred())
 		Expect(d).To(Equal(data{
-			String: "str!",
-			Bytes:  []byte("bytes!"),
-			Int:    123,
-			Uint:   456,
-			Float:  123.456,
-			Bool:   true,
+			String:  "str!",
+			Bytes:   []byte("bytes!"),
+			Int:     123,
+			Int64:   123456789123456789,
+			Uint:    456,
+			Uint64:  987654321987654321,
+			Float:   123.456,
+			Float64: 1.2345678912345678e+17,
+			Bool:    true,
 		}))
 
 		// Scan a different type with the same values to test that
@@ -85,12 +143,15 @@ var _ = Describe("Scan", func() {
 
 		Expect(Scan(&d, i{"string", "float", "bool"}, i{"", "1", "t"})).NotTo(HaveOccurred())
 		Expect(d).To(Equal(data{
-			String: "",
-			Bytes:  []byte("bytes!"),
-			Int:    123,
-			Uint:   456,
-			Float:  1.0,
-			Bool:   true,
+			String:  "",
+			Bytes:   []byte("bytes!"),
+			Int:     123,
+			Int64:   123456789123456789,
+			Uint:    456,
+			Uint64:  987654321987654321,
+			Float:   1.0,
+			Float64: 1.2345678912345678e+17,
+			Bool:    true,
 		}))
 	})
 
