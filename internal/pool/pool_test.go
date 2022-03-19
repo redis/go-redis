e8ad1fe1aa299b0c170c6c216f5d4c8ef00d15eb
zhangnaiqian <naiqianz@sina.cn>
2022-03-19 23:36:54 +0800

    fix: conncheck ut

--- internal/pool/pool_test.go
+++ internal/pool/pool_test.go
@@ -371,6 +371,8 @@ var _ = Describe("conns reaper", func() {
 					cn.SetUsedAt(time.Now().Add(-2 * idleTimeout))
 				case "aged":
 					cn.SetCreatedAt(time.Now().Add(-2 * maxAge))
+				case "connCheck":
+					_ = cn.Close()
 				}
 				conns = append(conns, cn)
 				staleConns = append(staleConns, cn)
@@ -457,6 +459,7 @@ var _ = Describe("conns reaper", func() {
 
 	assert("idle")
 	assert("aged")
+	assert("connCheck")
 })
 
 var _ = Describe("race", func() {
