c0a6f2d724206dcddade8359e8559cc93f528a2b
Leo Antunes <leo@costela.net>
2022-09-16 13:18:55 +0200

    chore(redisotel): bump deps

--- extra/redisotel/redisotel.go
+++ extra/redisotel/redisotel.go
@@ -6,7 +6,7 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
-	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.10.0"
 	"go.opentelemetry.io/otel/trace"
 
 	"github.com/go-redis/redis/extra/rediscmd/v9"
