515043aaab9e06b0216f79472b628e76802a5161
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2023-01-30 10:10:49 +0200

    chore: fix test

--- extra/redisotel/redisotel_test.go
+++ extra/redisotel/redisotel_test.go
@@ -34,32 +34,6 @@ func TestNewWithTracerProvider(t *testing.T) {
 	}
 }
 
-func TestNewWithAttributes(t *testing.T) {
-	provider := sdktrace.NewTracerProvider()
-	hook := newTracingHook("", WithTracerProvider(provider), WithAttributes(semconv.NetPeerNameKey.String("localhost")))
-	ctx, span := provider.Tracer("redis-test").Start(context.TODO(), "redis-test")
-	cmd := redis.NewCmd(ctx, "ping")
-	defer span.End()
-
-	processHook := hook.ProcessHook(func(ctx context.Context, cmd redis.Cmder) error {
-		attrs := trace.SpanFromContext(ctx).(sdktrace.ReadOnlySpan).Attributes()
-		if !(attrs[0] == semconv.DBSystemRedis) {
-			t.Fatalf("expected attrs[0] to be semconv.DBSystemRedis, got: %v", attrs[0])
-		}
-		if !(attrs[1] == semconv.NetPeerNameKey.String("localhost")) {
-			t.Fatalf("expected attrs[1] to be semconv.NetPeerNameKey.String(\"localhost\"), got: %v", attrs[1])
-		}
-		if !(attrs[2] == semconv.DBStatementKey.String("ping")) {
-			t.Fatalf("expected attrs[2] to be semconv.DBStatementKey.String(\"ping\"), got: %v", attrs[2])
-		}
-		return nil
-	})
-	err := processHook(ctx, cmd)
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
 func TestWithDBStatement(t *testing.T) {
 	provider := sdktrace.NewTracerProvider()
 	hook := newTracingHook(
