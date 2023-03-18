a20aa56c93d3604f794b752394a318746bfed197
ktsivkov <ktsivkov@gmail.com>
2023-03-18 16:16:14 +0100

    Added support for MODULE LOADEX command

--- commands.go
+++ commands.go
@@ -3684,3 +3684,32 @@ func (c cmdable) GeoPos(ctx context.Context, key string, members ...string) *Geo
 	_ = c(ctx, cmd)
 	return cmd
 }
+
+// LoadexConfig struct is used to specify the arguments for the MODULE LOADEX command of redis.
+// `MODULE LOADEX path [CONFIG name value [CONFIG name value ...]] [ARGS args [args ...]]`
+type LoadexConfig struct {
+	Path string
+	Conf map[string]interface{}
+	Args []interface{}
+}
+
+func (c *LoadexConfig) ToArgs() []interface{} {
+	var args = make([]interface{}, 3, len(c.Conf)*2+len(c.Args)+3)
+	args[0] = "MODULE"
+	args[1] = "LOADEX"
+	args[2] = c.Path
+	for k, v := range c.Conf {
+		args = append(args, "CONFIG", k, v)
+	}
+	for _, arg := range c.Args {
+		args = append(args, "ARGS", arg)
+	}
+	return args
+}
+
+// Loadex Redis `MODULE LOADEX path [CONFIG name value [CONFIG name value ...]] [ARGS args [args ...]]` command.
+func (c cmdable) Loadex(ctx context.Context, conf *LoadexConfig) *Cmd {
+	cmd := NewCmd(ctx, conf.ToArgs()...)
+	_ = c(ctx, cmd)
+	return cmd
+}
