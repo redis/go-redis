2da69f2349c06ed6b74a2d2beeb60fce14e04ad6
ktsivkov <ktsivkov@gmail.com>
2023-04-07 17:36:50 +0200

    Allocate the correct amount of memory
    Co-authored-by: Anurag Bandyopadhyay <angbpy@gmail.com>

--- commands.go
+++ commands.go
@@ -3885,7 +3885,7 @@ type ModuleLoadexConfig struct {
 }
 
 func (c *ModuleLoadexConfig) ToArgs() []interface{} {
-	var args = make([]interface{}, 3, len(c.Conf)*2+len(c.Args)+3)
+	args := make([]interface{}, 3, 3+len(c.Conf)*3+len(c.Args)*2)
 	args[0] = "MODULE"
 	args[1] = "LOADEX"
 	args[2] = c.Path
