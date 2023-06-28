646d99e12ac5470156a011d5deead117921c5573
ljun20160606 <ljun20160606@gmail.com>
2023-06-28 10:34:43 +0800

    feat: disables sending HELLO if protocol is -1

--- redis.go
+++ redis.go
@@ -280,24 +280,27 @@ func (c *baseClient) initConn(ctx context.Context, cn *pool.Conn) error {
 
 	var auth bool
 	protocol := c.opt.Protocol
-	// By default, use RESP3 in current version.
-	if protocol < 2 {
-		protocol = 3
-	}
+	// disables sending HELLO if protocol is lower than 0.
+	if protocol >= 0 {
+		// By default, use RESP3 in current version.
+		if protocol < 2 {
+			protocol = 3
+		}
 
-	// for redis-server versions that do not support the HELLO command,
-	// RESP2 will continue to be used.
-	if err := conn.Hello(ctx, protocol, username, password, "").Err(); err == nil {
-		auth = true
-	} else if !isRedisError(err) {
-		// When the server responds with the RESP protocol and the result is not a normal
-		// execution result of the HELLO command, we consider it to be an indication that
-		// the server does not support the HELLO command.
-		// The server may be a redis-server that does not support the HELLO command,
-		// or it could be DragonflyDB or a third-party redis-proxy. They all respond
-		// with different error string results for unsupported commands, making it
-		// difficult to rely on error strings to determine all results.
-		return err
+		// for redis-server versions that do not support the HELLO command,
+		// RESP2 will continue to be used.
+		if err := conn.Hello(ctx, protocol, username, password, "").Err(); err == nil {
+			auth = true
+		} else if !isRedisError(err) {
+			// When the server responds with the RESP protocol and the result is not a normal
+			// execution result of the HELLO command, we consider it to be an indication that
+			// the server does not support the HELLO command.
+			// The server may be a redis-server that does not support the HELLO command,
+			// or it could be DragonflyDB or a third-party redis-proxy. They all respond
+			// with different error string results for unsupported commands, making it
+			// difficult to rely on error strings to determine all results.
+			return err
+		}
 	}
 	if !c.opt.DisableIndentity {
 		libName := ""
