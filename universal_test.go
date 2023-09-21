8617e3cd0943d7b9227cb2c433cd3ea7487b597b
Tiago Peczenyj <tpeczenyj@weborama.com>
2023-09-21 14:36:59 +0200

    should be able to create a redis ring from universal options and universal ctor
    Signed-off-by: Tiago Peczenyj <tpeczenyj@weborama.com>

--- universal_test.go
+++ universal_test.go
@@ -38,4 +38,13 @@ var _ = Describe("UniversalClient", func() {
 		})
 		Expect(client.Ping(ctx).Err()).NotTo(HaveOccurred())
 	})
+
+	It("should connect to ring", func() {
+		ring := redisRingOptions()
+
+		client = redis.NewUniversalClient(&redis.UniversalOptions{
+			AddressMap: ring.Addrs,
+		})
+		Expect(client.Ping(ctx).Err()).NotTo(HaveOccurred())
+	})
 })
