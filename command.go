21ed15bbed2774a40fe831f7618332aa4f207fd1
Tiago Peczenyj <tpeczenyj@weborama.com>
2024-02-14 21:40:20 +0100

    Add helpers to set libinfo without panic (#2724)
    * add helpers to set library name and library info without risk of panic if we try to set both
    
    * refactor code to use helpers
    
    * add example
    
    * refactor tests
    
    * fix testable example
    
    * simplify example
    
    * rename exampl
    
    * fix ring.go
    
    * update example
    
    ---------
    
    Co-authored-by: ofekshenawa <104765379+ofekshenawa@users.noreply.github.com>

--- command.go
+++ command.go
@@ -5310,6 +5310,16 @@ type LibraryInfo struct {
 	LibVer  *string
 }
 
+// WithLibraryName returns a valid LibraryInfo with library name only.
+func WithLibraryName(libName string) LibraryInfo {
+	return LibraryInfo{LibName: &libName}
+}
+
+// WithLibraryVersion returns a valid LibraryInfo with library version only.
+func WithLibraryVersion(libVer string) LibraryInfo {
+	return LibraryInfo{LibVer: &libVer}
+}
+
 // -------------------------------------------
 
 type InfoCmd struct {
