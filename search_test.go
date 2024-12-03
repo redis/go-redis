d5c2272e4909b90dffd6192c3f80a99cf2cd4d4c
Christopher Golling <Chris.Golling@aexp.com>
2024-12-03 14:47:55 -0700

    Another FTSearch test fix

--- search_test.go
+++ search_test.go
@@ -120,14 +120,14 @@ var _ = Describe("RediSearch commands Resp 2", Label("search"), func() {
 
 		res2, err := client.FTSearchWithArgs(ctx, "num", "foo", &redis.FTSearchOptions{NoContent: true, SortBy: []redis.FTSearchSortBy{sortBy2}}).Result()
 		Expect(err).NotTo(HaveOccurred())
-		Expect(res2.Total).To(BeEquivalentTo(int64(0)))
+		Expect(res2.Total).To(BeEquivalentTo(int64(3)))
 		Expect(res2.Docs[2].ID).To(BeEquivalentTo("doc1"))
 		Expect(res2.Docs[1].ID).To(BeEquivalentTo("doc2"))
 		Expect(res2.Docs[0].ID).To(BeEquivalentTo("doc3"))
 
 		res3, err := client.FTSearchWithArgs(ctx, "num", "foo", &redis.FTSearchOptions{NoContent: true, SortBy: []redis.FTSearchSortBy{sortBy2}, SortByWithCount: true}).Result()
 		Expect(err).NotTo(HaveOccurred())
-		Expect(res3.Total).To(BeEquivalentTo(int64(3)))
+		Expect(res3.Total).To(BeEquivalentTo(int64(0)))
 		Expect(res3.Docs[2].ID).To(BeEquivalentTo("doc1"))
 		Expect(res3.Docs[1].ID).To(BeEquivalentTo("doc2"))
 		Expect(res3.Docs[0].ID).To(BeEquivalentTo("doc3"))
