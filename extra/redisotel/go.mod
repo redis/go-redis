dffbe613abc88f162b59fca2ac20db1dd56f6193
Jatin Rungta <9137405+urdarinda@users.noreply.github.com>
2024-12-17 00:55:43 +0530

    Add tests

--- extra/redisotel/go.mod
+++ extra/redisotel/go.mod
@@ -9,20 +9,23 @@ replace github.com/redis/go-redis/extra/rediscmd/v9 => ../rediscmd
 require (
 	github.com/redis/go-redis/extra/rediscmd/v9 v9.6.2
 	github.com/redis/go-redis/v9 v9.6.2
-	go.opentelemetry.io/otel v1.22.0
-	go.opentelemetry.io/otel/metric v1.22.0
-	go.opentelemetry.io/otel/sdk v1.22.0
-	go.opentelemetry.io/otel/trace v1.22.0
+	github.com/stretchr/testify v1.10.0
+	go.opentelemetry.io/otel v1.23.0
+	go.opentelemetry.io/otel/metric v1.23.0
+	go.opentelemetry.io/otel/sdk v1.23.0
+	go.opentelemetry.io/otel/sdk/metric v1.23.0
+	go.opentelemetry.io/otel/trace v1.23.0
 )
 
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
-retract (
-	v9.5.3 // This version was accidentally released.
-)
+retract v9.5.3 // This version was accidentally released.
---
+++ extra/redisotel/metrics_test.go
@@ -0,0 +1,148 @@
+package redisotel
+
+import (
+	"context"
+	"testing"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/sdk/instrumentation"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/metric/metricdata"
+	"go.opentelemetry.io/otel/sdk/metric/metricdata/metricdatatest"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+)
+
+var instrumentationScope = instrumentation.Scope{
+	Name:    instrumName,
+	Version: "semver:" + redis.Version(),
+}
+
+func setupMetrics(conf *config) (*sdkmetric.ManualReader, *redis.Client) {
+	reader := sdkmetric.NewManualReader()
+	mp := sdkmetric.NewMeterProvider(sdkmetric.WithReader(reader))
+	otel.SetMeterProvider(mp)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: ":6379",
+	})
+	if conf.meter == nil {
+		conf.meter = conf.mp.Meter(
+			instrumName,
+			metric.WithInstrumentationVersion("semver:"+redis.Version()),
+		)
+	}
+	addMetricsHook(rdb, conf)
+	return reader, rdb
+}
+
+func TestMetrics(t *testing.T) {
+	reader, rdb := setupMetrics(newConfig())
+	rdb.Get(context.Background(), "key")
+
+	want := metricdata.ScopeMetrics{
+		Scope: instrumentationScope,
+		Metrics: []metricdata.Metrics{
+			{
+				Name:        "db.client.connections.create_time",
+				Description: "The time it took to create a new connection.",
+				Unit:        "ms",
+				Data: metricdata.Histogram[float64]{
+					Temporality: metricdata.CumulativeTemporality,
+					DataPoints: []metricdata.HistogramDataPoint[float64]{
+						{
+							Attributes: attribute.NewSet(
+								semconv.DBSystemRedis,
+								attribute.String("status", "error"),
+							),
+						},
+					},
+				},
+			},
+			{
+				Name:        "db.client.connections.use_time",
+				Description: "The time between borrowing a connection and returning it to the pool.",
+				Unit:        "ms",
+				Data: metricdata.Histogram[float64]{
+					Temporality: metricdata.CumulativeTemporality,
+					DataPoints: []metricdata.HistogramDataPoint[float64]{
+						{
+							Attributes: attribute.NewSet(
+								semconv.DBSystemRedis,
+								attribute.String("type", "command"),
+								attribute.String("status", "error"),
+							),
+						},
+					},
+				},
+			},
+		},
+	}
+	rm := metricdata.ResourceMetrics{}
+	err := reader.Collect(context.Background(), &rm)
+	assert.NoError(t, err)
+	require.Len(t, rm.ScopeMetrics, 1)
+	metricdatatest.AssertEqual(t, want, rm.ScopeMetrics[0], metricdatatest.IgnoreTimestamp(), metricdatatest.IgnoreValue())
+}
+
+func TestCustomAttributes(t *testing.T) {
+	customAttrFn := func(ctx context.Context) []attribute.KeyValue {
+		return []attribute.KeyValue{
+			attribute.String("custom", "value"),
+		}
+	}
+	config := newConfig(WithAttributesFunc(customAttrFn))
+	reader, rdb := setupMetrics(config)
+
+	rdb.Get(context.Background(), "key")
+
+	want := metricdata.ScopeMetrics{
+		Scope: instrumentationScope,
+		Metrics: []metricdata.Metrics{
+			{
+				Name:        "db.client.connections.create_time",
+				Description: "The time it took to create a new connection.",
+				Unit:        "ms",
+				Data: metricdata.Histogram[float64]{
+					Temporality: metricdata.CumulativeTemporality,
+					DataPoints: []metricdata.HistogramDataPoint[float64]{
+						{
+							Attributes: attribute.NewSet(
+								semconv.DBSystemRedis,
+								attribute.String("status", "error"),
+							),
+						},
+					},
+				},
+			},
+			{
+				Name:        "db.client.connections.use_time",
+				Description: "The time between borrowing a connection and returning it to the pool.",
+				Unit:        "ms",
+				Data: metricdata.Histogram[float64]{
+					Temporality: metricdata.CumulativeTemporality,
+					DataPoints: []metricdata.HistogramDataPoint[float64]{
+						{
+							Attributes: attribute.NewSet(
+								semconv.DBSystemRedis,
+								attribute.String("type", "command"),
+								attribute.String("status", "error"),
+								attribute.String("custom", "value"),
+							),
+						},
+					},
+				},
+			},
+		},
+	}
+	rm := metricdata.ResourceMetrics{}
+	err := reader.Collect(context.Background(), &rm)
+	assert.NoError(t, err)
+	require.Len(t, rm.ScopeMetrics, 1)
+	metricdatatest.AssertEqual(t, want, rm.ScopeMetrics[0], metricdatatest.IgnoreTimestamp(), metricdatatest.IgnoreValue())
+}
