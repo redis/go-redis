a604ea5702397cb430543d270393e45e50fcdff2
Nedyalko Dyakov <nedyalko.dyakov@gmail.com>
2025-02-07 12:09:49 +0200

    fix: add unstableresp3 to cluster client (#3266)
    * fix: add unstableresp3 to cluster client
    
    * propagate unstableresp3
    
    * proper test that will ignore error, but fail if client panics
    
    * add separate test for clusterclient constructor

--- osscluster.go
+++ osscluster.go
@@ -90,6 +90,9 @@ type ClusterOptions struct {
 	DisableIndentity bool // Disable set-lib on connect. Default is false.
 
 	IdentitySuffix string // Add suffix to client name. Default is empty.
+
+	// UnstableResp3 enables Unstable mode for Redis Search module with RESP3.
+	UnstableResp3 bool
 }
 
 func (opt *ClusterOptions) init() {
@@ -304,7 +307,8 @@ func (opt *ClusterOptions) clientOptions() *Options {
 		// much use for ClusterSlots config).  This means we cannot execute the
 		// READONLY command against that node -- setting readOnly to false in such
 		// situations in the options below will prevent that from happening.
-		readOnly: opt.ReadOnly && opt.ClusterSlots == nil,
+		readOnly:      opt.ReadOnly && opt.ClusterSlots == nil,
+		UnstableResp3: opt.UnstableResp3,
 	}
 }
 
