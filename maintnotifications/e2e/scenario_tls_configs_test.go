2b727dd6e42db09b6db0ca3c60ead4028cf0cfce
kiryazovi-redis <ivaylo.kiryazov@redis.com>
2025-12-03 14:14:21 +0200

    test: reduce flakiness in e2e tests
    - Increase traffic analysis duration from 30s to 60s in endpoint types test to allow sufficient time for push notification analysis
    - Fix TLS config test function name by replacing Cyrillic 'Т' with ASCII 'T' to ensure proper test discovery

--- maintnotifications/e2e/scenario_tls_configs_test.go
+++ maintnotifications/e2e/scenario_tls_configs_test.go
@@ -15,7 +15,7 @@ import (
 
 // TODO ADD TLS CONFIGS
 // TestTLSConfigurationsPushNotifications tests push notifications with different TLS configurations
-func ТestTLSConfigurationsPushNotifications(t *testing.T) {
+func TestTLSConfigurationsPushNotifications(t *testing.T) {
 	if os.Getenv("E2E_SCENARIO_TESTS") != "true" {
 		t.Skip("Scenario tests require E2E_SCENARIO_TESTS=true")
 	}
