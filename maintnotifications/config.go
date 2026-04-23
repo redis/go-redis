4e12c6d759cb26f0ef719c6f3fdc7767faa5b854
Nedyalko Dyakov <nedyalko.dyakov@gmail.com>
2026-04-23 16:05:00 +0300

    fix log level check

--- maintnotifications/config.go
+++ maintnotifications/config.go
@@ -438,7 +438,7 @@ func DetectEndpointType(addr string, tlsEnabled bool) EndpointType {
 
 		isInternal, err := isInternalHostname(ctx, host)
 		// Will fallback to external FQDN if we can't determine if it's internal
-		if err != nil && logs.WarnOrAbove(internal.Logger) {
+		if err != nil && internal.LogLevel.WarnOrAbove() {
 			internal.Logger.Printf(ctx, "Failed to determine if hostname %q is internal: %v", host, err)
 		}
 
