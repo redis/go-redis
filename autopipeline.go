738818481f1543a4c371f8aed0071feefbadda84
cxljs <fukua95@gmail.com>
2026-06-11 22:02:49 +0800

    fix(autopipeline): return undersized slice to pool in getQueueSlice (#3846)
    * fix(autopipeline): return undersized slice to pool in getQueueSlice
    
    When the pooled slice capacity is too small, getQueueSlice allocates a
    new slice but discards the old one without returning it to the pool.
    
    * remove dead code

--- autopipeline.go
+++ autopipeline.go
@@ -138,6 +138,7 @@ func getQueueSlice(capacity int) []*queuedCmd {
 	slice = slice[:0]
 	// If the capacity is too small, allocate a new one
 	if cap(slice) < capacity {
+		queueSlicePool.Put(slice)
 		return make([]*queuedCmd, 0, capacity)
 	}
 	return slice
@@ -473,11 +474,6 @@ func (ap *AutoPipeliner) flushBatchSlice() {
 			putQueueSlice(queuedCmds)
 			return
 		}
-	}
-
-	if len(queuedCmds) == 0 {
-		ap.sem.Release()
-		return
 	}
 
 	// Fast path for single command
